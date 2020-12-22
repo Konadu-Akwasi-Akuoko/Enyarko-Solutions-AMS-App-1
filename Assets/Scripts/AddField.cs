6cc879e8e7a847e4ba40a1ea0cf0e654da48b479
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2020-12-22 17:20:59 +0000

    Second Commit, UI and Scrollable window

---
+++ Assets/Scripts/AddField.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddField : MonoBehaviour
+{
+    public GameObject InputField;
+    public int InputFieldNumber =default;
+
+    public void AddNewInputField()
+    {
+        InputFieldNumber++;
+        GameObject newInputField = Instantiate(InputField, transform);
+        newInputField.name = "Input Field " + InputFieldNumber.ToString();
+    }
+
+}
